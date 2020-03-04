2c4e62ba7af886968b00d7bf278edf8a2e8dcf86
Lukas Hedegaard Jensen <au478108@d40049.st.client.au.dk>
2020-03-04 09:11:48 +0100

    add folder loaders with tests

--- src/mldatasets/loaders/loaders.py
+++ src/mldatasets/loaders/loaders.py
@@ -1,3 +1,61 @@
+from pathlib import Path
+from glob import glob
+from mldatasets.datasets.base import ItemGetter
+from mldatasets.datasets.dataset import Dataset
+from typing import Callable, Any, Union
+# FolderDataItemGetter
 
-def load_dataset(path):
-    pass
+AnyPath = Union[str, Path]
+
+
+class DataGetter(ItemGetter):
+
+    def __init__(self, getdata:Callable[[Any], Any], description='data getter'):
+        self._getdata = getdata
+        self._description = description
+
+    def __getitem__(self, i: int):
+        return self._getdata(i)
+
+
+
+def load_folder_data(path: AnyPath) -> Dataset:
+    p = Path(path)
+    ids = [str(x.relative_to(p)) for x in p.glob('[!._]*')]
+
+    def get_data(i):
+        nonlocal p
+        return str(p/i)
+
+    datagetter = DataGetter(get_data, "Data Getter for folder with structure 'root/data'")
+
+    ds = Dataset(downstream_getter=datagetter)
+    ds._extend(ids)
+
+    return ds
+
+
+
+def load_folder_class_data(path: AnyPath) -> Dataset:
+    p = Path(path)
+    classes = [x for x in p.glob('[!._]*')]
+
+    def get_data(i):
+        nonlocal p
+        return str(p/i)
+
+    datagetter = DataGetter(get_data, "Data Getter for folder with structure 'root/classes/data'")
+
+    ds = Dataset(downstream_getter=datagetter)
+    for c in classes:
+        ids = [str(x.relative_to(p)) for x in c.glob('[!._]*')]
+        ds._extend(ids)
+
+    return ds
+
+
+def load_folder_dataset_class_data(path: AnyPath) -> Dataset:
+    p = Path(path)
+    dataset_paths = [x for x in p.glob('[!._]*')]
+
+    return [load_folder_class_data(dsp) for dsp in dataset_paths]
--- tests/mldatasets/loaders/test_loaders.py
+++ tests/mldatasets/loaders/test_loaders.py
@@ -1,2 +1,66 @@
-def test_loaders():
-    pass
+from pathlib import Path
+import mldatasets.loaders as loaders
+
+from pathlib import Path
+from enum import Enum
+
+# utils ##########################
+
+class TestDatasets(Enum):
+    FOLDER_DATA = 'folder_dataset_class_data/amazon/back_pack'
+    FOLDER_CLASS_DATA = 'folder_dataset_class_data/amazon'
+    FOLDER_DATASET_CLASS_DATA = 'folder_dataset_class_data'
+    MAT_SINGLE_WITH_MULTI_DATA = 'mat_single_with_multi_data'
+
+
+def get_test_dataset_path(dataset_enum: TestDatasets) -> str:
+    return str((Path(__file__).parent.parent / 'recourses' / dataset_enum.value).absolute())
+
+
+# tests ##########################
+
+def test_folder_data():
+    path = get_test_dataset_path(TestDatasets.FOLDER_DATA)
+
+    expected_items = [str(Path(path)/f'frame_000{i}.jpg') for i in range(1,7)]
+
+    ds = loaders.load_folder_data(path)
+    found_items = [i for i in ds]
+
+    assert(set(expected_items) == set(found_items))
+
+
+def test_folder_class_data():
+    path = get_test_dataset_path(TestDatasets.FOLDER_CLASS_DATA)
+
+    expected_items = [str(p) for p in Path(path).glob('*/*.jpg')]
+
+    ds = loaders.load_folder_class_data(path)
+    found_items = [i for i in ds]
+
+    assert(set(expected_items) == set(found_items))
+
+
+
+def test_folder_dataset_class_data():
+    path = get_test_dataset_path(TestDatasets.FOLDER_DATASET_CLASS_DATA)
+    sets = Path(path).glob('[!._]*')
+
+    sets_of_expected_items = [
+        set([str(p) for p in Path(s).glob('*/*.jpg')])
+        for s in sets
+    ]
+
+    datasets = loaders.load_folder_dataset_class_data(path)
+    sets_of_found_items = [
+        set([i for i in ds])
+        for ds in datasets
+    ]
+
+    for expected_items_set in sets_of_expected_items:
+        assert(any([expected_items_set == found_items for found_items in sets_of_found_items]))
+
+
+# def mat_single_with_multi_data():
+#     path = get_test_dataset_path(TestDatasets.MAT_SINGLE_WITH_MULTI_DATA)
+#     assert(False)
