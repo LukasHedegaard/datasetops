d9d9edb4df0b0124690b59d5fa4cd20193eea632
LukasHedegaard <lh@eng.au.dk>
2020-03-19 15:26:51 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="datasetops",
     version="0.0.3",
+    author="Lukas Hedegaard",
+    description="Friendly dataset operations for your data science needs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LukasHedegaard/datasetops",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Pytest",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
