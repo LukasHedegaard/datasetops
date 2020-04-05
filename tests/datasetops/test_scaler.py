1315dfd533652e28b5b610e9c7755085a174d0e9
LukasHedegaard <lh@eng.au.dk>
2020-04-05 15:26:03 +0200

    Add center internal impl

--- tests/datasetops/test_scaler.py
+++ tests/datasetops/test_scaler.py
@@ -81,14 +81,47 @@ def test_standardize():
     for d in orig_data:
         scaler.fit(d)
 
-    # default range (0,1)
-    scale_std = scaler.standardize()  # default range
+    scale_std = scaler.standardize() 
     result = [scale_std(d) for d in orig_data]
     v = 1.224744871391589  # np.std([-1.224744871391589, 0, -1.224744871391589]) = 1
     expected = [
         ("1", -v * np.ones(SHAPE_1D), -v * np.ones(SHAPE_3D), -v, 10),
         ("2", 0 * np.ones(SHAPE_1D), 0 * np.ones(SHAPE_3D), 0, 20),
         ("3", v * np.ones(SHAPE_1D), v * np.ones(SHAPE_3D), v, 30),
+    ]
+    for r, e, o in zip(result, expected, orig_data):
+        assert r[0] == e[0] == o[0]
+        assert np.array_equal(r[1], e[1])
+        assert np.array_equal(r[2], e[2])
+        assert not np.array_equal(r[1], o[1])
+        assert not np.array_equal(r[2], o[2])
+        assert r[3] == e[3] != o[3]
+        assert r[4] == e[4] == o[4]
+
+
+def test_center():
+    SHAPE_1D = (2,)
+    SHAPE_3D = (5, 4, 3)
+    orig_data = [
+        # (string, 1D, 3D, scalar, scalar)
+        ("1", np.ones(SHAPE_1D), np.ones(SHAPE_3D), 1, 10),
+        ("2", 2 * np.ones(SHAPE_1D), 2 * np.ones(SHAPE_3D), 2, 20),
+        ("3", 3 * np.ones(SHAPE_1D), 3 * np.ones(SHAPE_3D), 3, 30),
+    ]
+
+    scaler = Scaler(
+        [None, ScalingInfo(SHAPE_1D), ScalingInfo(SHAPE_3D, 2), ScalingInfo()]
+    )  # omit last items
+
+    for d in orig_data:
+        scaler.fit(d)
+
+    scale_center = scaler.center() 
+    result = [scale_center(d) for d in orig_data]
+    expected = [
+        ("1", -1 * np.ones(SHAPE_1D), -1 * np.ones(SHAPE_3D), -1, 10),
+        ("2", 0 * np.ones(SHAPE_1D), 0 * np.ones(SHAPE_3D), 0, 20),
+        ("3", 1 * np.ones(SHAPE_1D), 1 * np.ones(SHAPE_3D), 1, 30),
     ]
     for r, e, o in zip(result, expected, orig_data):
         assert r[0] == e[0] == o[0]
